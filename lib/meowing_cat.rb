aa14237060f536bb228eb7ccf899784a3106042c
jasonodoom <jason@ohdoom.net>
2018-09-19 11:22:45 -0400

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,15 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+  attr_accessor :meow, :name # Createsa setter and getter method
+
+  def meow
+    puts "meow!"
+  end
+
+end
+
+maru = Cat.new
+maru.name = "Maru"
+
+maru.name
+maru.meow
